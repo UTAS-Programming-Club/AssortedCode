28a6560ac6fca94dadf87223253014dc340f6c8f
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2026-04-17 20:00:29 +1000

    Merge bat to exe creator files

--- bat2exe.bat
+++ bat2exe.bat
@@ -1,5 +1,7 @@
+<!-- : Begin batch script
 @ECHO OFF
 REM Based on https://github.com/amakvana/Bat-2-Exec/blob/5f75539/Bat2Exe.bat
+REM and https://stackoverflow.com/a/9074483
 
 IF "%~1" EQU "" EXIT
 IF "%~2" EQU "" EXIT
@@ -7,7 +9,7 @@ SET "target_exe=%__cd__%%~n2.exe"
 SET "tmp_exe=%target_exe%.tmp"
 SET "bat_name=%~nx2"
 SET "bat_dir=%~dp2"
-SET "sed=exe.sed"
+SET "sed=%temp%\exe.sed"
 
 (
   ECHO [Version]
@@ -32,12 +34,38 @@ SET "sed=exe.sed"
   ECHO %bat_name%=
 )>"%sed%"
 
-CALL iexpress /n /q /m %sed%
+CALL iexpress /M /N /Q %sed%
 
 IF "%~1" EQU "-t" (
-  CALL cscript //b fixexec.vbs
+  CALL cscript //B //Job:FixSubsystem "%~f0?.wsf" "%tmp_exe%" "%target_exe%"
 ) ELSE (
   COPY "%tmp_exe%" "%target_exe%" >NUL
 )
 
-DEL /q /f "%sed%" "%tmp_exe%"
+DEL /Q "%sed%" "%tmp_exe%"
+EXIT /B
+
+----- Begin wsf script --->
+<package>
+  <job id="FixSubsystem">
+    <script language="VBScript">
+      Dim fs, oldExec, newExec
+      Set fs = CreateObject("Scripting.FileSystemObject")
+      Set oldExec = fs.OpenTextFile(WScript.Arguments(0))
+      Set newExec = fs.OpenTextFile(WScript.Arguments(1), 2, True)
+      If IsNull(oldExec) or IsNull(newExec) Then
+        WScript.Quit
+      End If
+
+      newExec.Write(oldExec.Read(324))
+      newExec.Write(chr(3))
+      oldExec.Skip(1)
+      Do While oldExec.AtEndOfStream <> True
+        newExec.Write(oldExec.Read(100))
+      Loop
+
+      oldExec.Close()
+      newExec.Close()
+    </script>
+  </job>
+</package>
